daa1437c9c2cc3a52a889e2ea4e334c37e4c01fc
ShashwatHiregoudar <shashwathiregoudar@gmail.com>
2018-03-09 16:00:13 +0530

    succesfully searching from the database

--- db2.js
+++ db2.js
@@ -15,6 +15,11 @@ con.connect(function(err) {
   var ucrtn = "CREATE TABLE users (name varchar(32) , username varchar(32), address varchar(32), password varchar(32))"
   con.query(ucrtn, function (err, result) {
     if (err) throw err;
-    console.log("User created");
+    console.log("User table created");
+  });
+  var tracrtn = "CREATE TABLE transactions (applequantity int, bananaquantity int, orangequantity int, waterquantity int, chikkuquantity int);";
+  con.query(tracrtn, function (err, result) {
+    if (err) throw err;
+    console.log("Transaction table created");
   });
 });
